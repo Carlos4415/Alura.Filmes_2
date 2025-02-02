0e8053db21ed15f2701d23daf9ffc2835b555056
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-02 11:51:41 -0300

    Atualizando README

--- Program.cs
+++ Program.cs
@@ -2,9 +2,6 @@
 // A classe "Filme" representa um filme com propriedades como título, duração e elenco. 
 // A classe "Artista" representa uma pessoa que atua em filmes, com atributos como nome, idade e uma lista de filmes nos quais o artista participou. 
 // Ambas as classes são organizadas dentro do namespace "Alura.Filmes_2", visando estruturar e reutilizar o código em projetos relacionados a filmes.
-
-// PAREI EM
-// 3. Criar uma classe Artista, que representa uma pessoa que atua em filmes, no namespace Alura.Filmes. A classe deve conter atributos como o nome, idade e uma lista de filmes onde o artista atuou.
 using Alura.Filmes_2;
 
 Artista robertDowneyJr = new Artista("Robert Downey Jr", 59);
@@ -15,17 +12,17 @@ Artista scarlettJohansson = new Artista("Scarlett Johansson", 40);
 
 Filme sherlockHolmes = new Filme
 (
-    "Sherlock Holmes", 
-    128, 
+    "Sherlock Holmes",
+    128,
     new List<Artista>()
     {
         robertDowneyJr,
         judeLaw,
         rachelMcAdams,
-    } 
+    }
  );
 
-Filme vingadoresUltimato = new Filme("Vingadores: Ultimato", 181, new List<Artista>() { chrisEvans } );
+Filme vingadoresUltimato = new Filme("Vingadores: Ultimato", 181, new List<Artista>() { chrisEvans });
 
 vingadoresUltimato.AdicionarElenco(scarlettJohansson);
 robertDowneyJr.AdicionarFilme(vingadoresUltimato);
