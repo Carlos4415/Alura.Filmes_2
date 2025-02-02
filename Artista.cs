c5c6de34fd6648c730afa0dad960112d6065af51
Carlos4415 <carlos.soares4415@gmail.com>
2025-02-02 11:26:37 -0300

    first commit

---
+++ Artista.cs
@@ -0,0 +1,39 @@
+﻿namespace Alura.Filmes_2;
+
+public class Artista
+{
+    public string Nome { get; set; }
+    public int Idade { get; set; }
+    public List<Filme> FilmesAtuados { get; set; }
+    public int QuantidadeFilmes => FilmesAtuados.Count;
+
+    public Artista(string nome, int idade)
+    {
+        Nome = nome;
+        Idade = idade;
+        FilmesAtuados = new List<Filme>();
+    }
+
+    public void AdicionarFilme(Filme filme)
+    {
+        FilmesAtuados.Add(filme);
+
+        if (!filme.Elenco.Contains(this)) filme.AdicionarElenco(this);
+    }
+
+    public void MostrarFilmesAtuados()
+    {
+        if (this.FilmesAtuados.Count == 0)
+        {
+            Console.WriteLine($"\nNenhum filme encontrado na base para {this.Nome}");
+            return;
+        }
+
+        Console.WriteLine($"\nFilmes de {this.Nome}...");
+
+        foreach (var filme in FilmesAtuados)
+        {
+            Console.WriteLine($"Filme: {filme.Titulo}");
+        }
+    }
+}
---
+++ Filme.cs
@@ -0,0 +1,57 @@
+﻿namespace Alura.Filmes_2;
+
+public class Filme
+{
+    public List<Artista> Elenco { get; set; }
+    public string Titulo { get; set; }
+    public int Duracao { get; set; }
+
+    public Filme(string titulo, int duracao, List<Artista> elenco)
+    {
+        if (elenco == null)
+        {
+            Elenco = new List<Artista>();
+        }
+        else
+        {
+            Elenco = elenco;
+
+            foreach (var artista in Elenco)
+            {
+                artista.AdicionarFilme(this);
+            }
+        }
+
+        Titulo = titulo;
+        Duracao = duracao;
+    }
+
+    public void AdicionarElenco(Artista artista) 
+    { 
+        Elenco.Add(artista);
+
+        if (!artista.FilmesAtuados.Contains(this))
+        {
+            artista.AdicionarFilme(this);
+        }
+
+        Console.WriteLine($"\n{artista.Nome} adicionado/a ao elenco.");
+    }
+
+    public void ListarElenco()
+    {
+        if (Elenco.Count == 0)
+        {
+            Console.WriteLine("\nElenco vazio.");
+        }
+        else
+        {
+            Console.WriteLine($"\n{this.Titulo}: Elenco...");
+
+            foreach (var artista in Elenco)
+            {
+                Console.WriteLine(artista.Nome);   
+            }
+        }
+    }
+}
